7a53984b5d28c34ce423a58e6d99b0509dcf965d
AlnisS <alnis.smidchens@gmail.com>
2021-11-09 20:30:02 -0800

    first person driving

--- cart/Master.gd
+++ cart/Master.gd
@@ -24,13 +24,24 @@ func _physics_process(delta):
 	$BounceEffect/BounceInner.radius = bounce_progress * 8 - 0.2
 	$BounceEffect.material_override.albedo_color = Color(1.0, 1.0, 1.0, pow(1.0 - bounce_progress, 3))
 	
-	$PlayerCamera.transform = $PlayerCamera.transform.interpolate_with($GolfCart/TargetCamera.get_global_transform(), 0.2)
+	var view_displacement = $PlayerCamera.transform.origin - $GolfCart/TargetCamera.get_global_transform().origin
+	var view_distance = view_displacement.length()
+	var extra_lerp = clamp((view_distance - 0.3) * 3, 0.0, 0.7)
+#	var extra_lerp = 0.2
+#	print(extra_lerp)
+#	print(view_distance)
+	
+	$PlayerCamera.transform = $PlayerCamera.transform.interpolate_with($GolfCart/TargetCamera.get_global_transform(), 0.3 + extra_lerp)
+	
 	var steer_target = 0.0
 	if Input.is_action_pressed("steer_left"):
 		steer_target += 0.6
 	if Input.is_action_pressed("steer_right"):
 		steer_target -= 0.6
 	$GolfCart.steering = lerp($GolfCart.steering, steer_target, 0.1)
+	
+	$GolfCart/Chassis/SteeringBase/SteeringWheel.rotation.y = $GolfCart.steering * 2
+	print($GolfCart.steering)
 	
 	if Input.is_action_pressed("gas"):
 		$GolfCart.engine_force = 150.0
